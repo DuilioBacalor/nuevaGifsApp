3316e25603e0d351964bf7c61cf41374a78b4c69
Duilio <duiliobacalorwork>
2022-07-06 14:45:38 -0300

    appGifsFuncional

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 // ymLltV1dpkS9w5wR1Ae6X5mR2ny402CG
 @Injectable({
@@ -6,10 +7,17 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 private apiKey='ymLltV1dpkS9w5wR1Ae6X5mR2ny402CG';
  private _historial:string[]=[];
+//  TODO: Cambiar any por su tipo correspondiente
+ public resultados:any;
 
  get historial(){
 
   return [...this._historial];
+ }
+
+ constructor(private http:HttpClient){
+  
+
  }
 
  buscarGifs( query:string){
@@ -18,6 +26,14 @@ private apiKey='ymLltV1dpkS9w5wR1Ae6X5mR2ny402CG';
 if(!this._historial.includes(query)){
   this._historial.unshift(query);
   this._historial=this._historial.splice(0,10);
+  this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=ymLltV1dpkS9w5wR1Ae6X5mR2ny402CG&q=${query} z&limit=10`)
+  .subscribe((resp:any)=>{
+    console.log(resp.data);
+    this.resultados=resp.data;
+  });
+
+
+
 }
 
 console.log(this._historial);
